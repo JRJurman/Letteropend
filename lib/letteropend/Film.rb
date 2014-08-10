233a2b95f5f9efc4abdd66fd8e9baaef4a91e383
Jesse Jurman <j.r.jurman@gmail.com>
2014-08-09 21:06:42 -0400

    finalized list.rb

--- lib/letteropend/Film.rb
+++ lib/letteropend/Film.rb
@@ -1,4 +1,5 @@
 class Film
+  attr_reader :title, :url
   def initialize(title, url)
     @title = title
     @url = url
--- lib/letteropend/List.rb
+++ lib/letteropend/List.rb
@@ -1,29 +1,38 @@
 require 'nokogiri'
 require 'open-uri'
+require './Film.rb'
 
 class List
-  attr_reader :films
+  attr_reader :username, :list, :films, :pages
 
-  def initialize(username, list="films", callbacks)
+  def initialize(username, list="films", callbacks={})
     @username = username
     @list = list
-    @callbacks = callbacks
 
     @films = []
     @pages = ["/#{username}/#{list}/page/1/"]
     begin
       # get page using Nokogiri
+      if callbacks[:new_page]
+        callbacks[:new_page].call(self)
+      end
       page = Nokogiri::HTML(open("http://www.letterboxd.com#{@pages.last}"))
 
       # append visible films to @films
-      doc.css(".poster").each do |film|
+      page.css(".poster").each do |film|
+        title = film.css(".frame-title").text
+        url = film.css("a")[0].attr("href")
+        @films.push( Film.new(title, url) )
+        if callbacks[:new_film]
+          callbacks[:new_film].call(self)
+        end
       end
 
       # see if there is a next page
-      next_page = page.css('.paginate-next')
-      if ( next_page.length == 1 )
-        @pages.push( next_page[0].attr("href") )
+      next_page = page.css('.paginate-next')[0].attr("href")
+      if ( next_page )
+        @pages.push( next_page )
       end
-    end while (next_page.length == 1)
+    end while ( next_page )
   end
 end
