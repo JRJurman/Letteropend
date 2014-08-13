3bb8d2b8a7413dbb77aa38b3df8d831ccaf1d5f4
Jesse Jurman <j.r.jurman@gmail.com>
2014-08-12 23:39:23 -0400

    removed colorized from examples

--- examples/compareWatchlist.rb
+++ examples/compareWatchlist.rb
@@ -1,9 +1,7 @@
-# This example requires colorize
-require "colorize"
 require_relative "../lib/letteropend"
 
 callbacks = {
-  :new_page => lambda{ |list| puts "Getting page ".light_yellow + list.pages.length.to_s.magenta + " from ".light_yellow + "#{list.username}'s #{list.list}".magenta }
+  :new_page => lambda{ |list| puts "Getting page {#{list.pages.length}} from #{list.username}'s #{list.list}" }
 }
 print "First username: "
 user1 = gets.chomp
@@ -16,5 +14,5 @@ user2_wl = Letteropend::List.new(user2, "watchlist", callbacks)
 
 user2_wl.films.delete_if { |film| !user1_wl.films.include? film }
 user2_wl.films.each do |film|
-  puts film.title.blue
+  puts "> #{film.title}"
 end
--- examples/getAllTime.rb
+++ examples/getAllTime.rb
@@ -1,5 +1,3 @@
-# This example requires colorize
-require "colorize"
 require_relative "../lib/letteropend"
 
 print "Enter username: "
@@ -11,25 +9,23 @@ ul = gets().chomp
 $ct = 0
 def pulled_film(film)
   $ct += film.runtime
-  puts "Adding ".cyan + film.runtime.to_s.blue.bold + " minutes; Total Minutes: ".cyan + $ct.to_s.blue.bold
+  puts "Adding #{film.runtime} minutes; Total Minutes: #{$ct}"
   if $ct.to_i < 1440
-    print "That's ".cyan + ($ct/60).to_s.blue.bold + " hours and ".cyan + ($ct%60).to_s.blue.bold + " minutes".cyan
+    puts "That's #{$ct/60} hours and #{$ct%60} minutes"
   elsif $ct.to_i < 525600
-    print "That's ".cyan + ($ct/1440).to_s.blue.bold + " days, ".cyan + ($ct%1440/60).to_s.blue.bold + " hours, and ".cyan + ($ct%1440%60).to_s.blue.bold + " minutes".cyan
+    puts "That's #{$ct/1440} days, #{$ct%1440/60} hours, and #{$ct%1440%60} minutes"
   else
-    print "That's ".cyan + ($ct/525600).to_s.blue.bold + " years, ".cyan + ($ct%525600/1440).to_s.blue.bold + " days, ".cyan + ($ct%545600%1440/60).to_s.blue.bold + " hours, and ".cyan + ($ct%535600%1440%60).to_s.blue.bold + " minutes".cyan
+    puts "That's #{$ct/525600} years, #{$ct%525600/1440} days, #{$ct%545600%1440/60} hours, and #{$ct%535600%1440%60} minutes"
   end
-
-  puts " -- around ".cyan + ($ct/90).to_s.blue.bold + " movies".cyan
 end
 
 callbacks = {
-  :new_page => lambda{ |l| puts "pulling data from ".yellow + l.pages.last.magenta.underline},
-  :pulling_film => lambda{ |f| puts "pulling data from ".yellow + f.url.blue.underline},
+  :new_page => lambda{ |l| puts "pulling data from {#{l.pages.last}}" },
+  :pulling_film => lambda{ |f| puts "pulling data from {#{f.url}}" },
   :pulled_film => lambda{ |f| Object.send(:pulled_film, f) }
 }
 
 l = Letteropend::List.new(un, ul, callbacks)
 total = l.get_total_time
 
-puts "Total Runtime: #{total}".blue
+puts "Total Runtime: #{total} minutes"
