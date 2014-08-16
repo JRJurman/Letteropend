8900fb5cce7cdc7bb1c221b891d29683e6a1d7ee
Nicolas McCurdy <thenickperson@gmail.com>
2014-08-16 02:52:53 -0400

    Add more pending specs

--- spec/film_spec.rb
+++ spec/film_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Letteropend::Film do
-  it 'has tests'
+  describe '.new' do
+    it 'defines @title'
+    it 'defines @url'
+    it 'defines @pulled'
+    it 'defines @callbacks'
+  end
+
+  describe '#runtime' do
+    it 'has tests'
+  end
+
+  describe '#tagline' do
+    it 'has tests'
+  end
+
+  describe '#overview' do
+    it 'has tests'
+  end
+
+  describe '#pull_data' do
+    it 'has tests'
+  end
+
+  describe '#==' do
+    it 'has tests'
+  end
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Letteropend::List do
-  it 'has tests'
+  describe '.new' do
+    it 'assigns @username'
+    it 'assigns @list'
+    it 'assigns @films'
+    it 'assigns @pages'
+  end
+
+  describe '#get_total_time' do
+    it 'returns the total time of all films'
+  end
 end
