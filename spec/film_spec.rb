88ed77ab6cfc53517a4ed9e9c488667f27a29cc0
Nicolas McCurdy <thenickperson@gmail.com>
2014-08-16 04:02:14 -0400

    Implement Film specs (except those involving callbacks)

--- spec/film_spec.rb
+++ spec/film_spec.rb
@@ -1,30 +1,60 @@
 require 'spec_helper'
 
 describe Letteropend::Film do
+  let(:film) { Letteropend::Film.new 'Chopping Mall', '/film/chopping-mall/' }
+
   describe '.new' do
-    it 'defines @title'
-    it 'defines @url'
-    it 'defines @pulled'
-    it 'defines @callbacks'
+    it('defines @title') { expect(film.title).to eq 'Chopping Mall' }
+
+    it('defines @url') { expect(film.url).to eq '/film/chopping-mall/' }
   end
 
   describe '#runtime' do
-    it
+    it('returns the running length') { expect(film.runtime).to be 77 }
   end
 
   describe '#tagline' do
-    it
+    it('returns the tagline') { expect(film.tagline).to eq 'Half-off is just the beginning!' }
   end
 
   describe '#overview' do
-    it
+    it('returns the overview') { expect(film.overview).to eq 'Eight teenagers are trapped after hours in a high tech shopping mall and pursued by three murderous security robots out of control.' }
   end
 
   describe '#pull_data' do
-    it
+    context 'without callbacks' do
+      it('defines @runtime') { expect(film.runtime).to be_truthy }
+
+      it('defines @tagline') { expect(film.tagline).to be_truthy }
+
+      it('defines @overview') { expect(film.overview).to be_truthy }
+    end
+
+    context 'with callbacks' do
+      it
+    end
   end
 
   describe '#==' do
-    it
+    let(:same_film) { Letteropend::Film.new 'Chopping Mall', '/film/chopping-mall/' }
+    let(:different_film) { Letteropend::Film.new 'Paul Blart: Mall Cop', '/film/paul-blart-mall-cop/' }
+    let(:different_name_film) { Letteropend::Film.new 'Paul Blart: Mall Cop', '/film/chopping-mall/' }
+    let(:different_url_film) { Letteropend::Film.new 'Chopping Mall', '/film/paul-blart-mall-cop/' }
+
+    context 'with the same film' do
+      it { expect(film == same_film).to be true }
+    end
+
+    context 'with a different film' do
+      it { expect(film == different_film).to be false }
+    end
+
+    context 'with a film with a different name' do
+      it { expect(film == different_name_film).to be false }
+    end
+
+    context 'with a film with a different url' do
+      it { expect(film == different_url_film).to be false }
+    end
   end
 end
